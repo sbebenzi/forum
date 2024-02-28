eb2a827c65d4ce0a9be2884a35d2185d07060709
Sarah Bz <sarahlover822@gmail.com>
2024-02-28 18:47:55 -0300

    iniciado projeto

---
+++ src/main/kotlin/br/com/alura/forum/HelloController.kt
@@ -0,0 +1,14 @@
+package br.com.alura.forum
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/hello")
+class HelloController {
+    @GetMapping
+    fun helloWorld():String{
+        return "Hello World Atual"
+    }
+}
